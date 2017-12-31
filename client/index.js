c4bda59b9a3821945d73ff046892916ba1339d68
Markus Mühlberger <markus@mmuehlberger.com>
2017-12-31 15:24:54 +0100

    Add Apollo dependencies and set up provider and client

--- client/index.js
+++ client/index.js
@@ -4,13 +4,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './components/app';
+
+const link = new HttpLink({ uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql' });
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
 
 const Root = () => {
   return (
-    <HashRouter>
-      <Route path="/" component={App} />
-    </HashRouter>
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <Route path="/" component={App} />
+      </HashRouter>
+    </ApolloProvider>
   );
 };
 
